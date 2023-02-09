f8b66eadf086cec4a3274fc10faf62e0aef3b4ef
RSSChenChu <shenchu.jin@edu.uah.es>
2023-02-09 16:08:28 +0100

    Pero pero y esta locura?

--- arcade-demo/main.py
+++ arcade-demo/main.py
@@ -100,7 +100,7 @@ class GameView(arcade.View):
             self.coin_list.append(coin)
 
     def on_show(self):
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.LIGHTBLUE)
 
         # Don't show the mouse cursor
         self.window.set_mouse_visible(True)
